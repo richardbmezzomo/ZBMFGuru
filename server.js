9a1fe8a8842ce97ffce573bb1071b7ccf91d2332
richardbmezzomo <richardmezzomo1@gmail.com>
2025-01-05 17:01:01 -0300

    connected MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import { connectDB } from './src/config/db.js'
 
 const app = express()
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('hello zbmf')
---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log(`Mongodb conectado: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Erro ao conectar ao MongoDB: ${error.message}`)
+    process.exit(1)
+  }
+}
